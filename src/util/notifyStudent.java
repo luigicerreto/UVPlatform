755bf6cf61c93cfdc5e73743d7875693d5e261a4
Antonio <Antonio@Desktop-Antonio>
2019-12-19 22:17:21 +0100

    Notifica allo studente completata

--- src/util/notifyStudent.java
+++ src/util/notifyStudent.java
@@ -13,6 +13,7 @@ import org.springframework.mock.web.MockHttpServletResponse;
 
 import controller.Downloader;
 import model.Attached;
+import model.SystemAttribute;
 import model_uvp.DAORichiesta;
 import model_uvp.RequestInternship;
 
@@ -25,29 +26,18 @@ public class notifyStudent {
 
 	public void notify(String mail, int idRequest) throws IOException, ServletException
 	{
-		response = new MockHttpServletResponse();
-		request = new MockHttpServletRequest();
-		servlet = new Downloader();
 		String id = String.valueOf(idRequest);
-		request.addParameter("idRequest", id);
-		
-		
 		Attached file = new Attached();
 		DAORichiesta queryobj = new DAORichiesta();
+		
 		text+=queryobj.checkStatus(idRequest);
 		file = queryobj.retriveAttached(idRequest);
-		if(file.equals(null))
-		{
-			Mailer.send(mail,object,text,"");  
-		}
-		else
-		{
-			request.addParameter("filename", file.getFilename());
-			System.out.println("il file name è "+file.getFilename());
-			servlet.doGet(request, response);
-			System.out.println(response.getContentAsString());
-			Mailer.send(mail,object,text,file.getFilename());  
-		}
+		String basePath = 
+		        new SystemAttribute().getValueByKey("request-upload-path") + "\\" + idRequest + "\\";
+		System.out.println("Il percorso è "+basePath+file.getFilename());
+		Mailer.send(mail,object,text,basePath+file.getFilename());  
+		
+		
 	}
 
 }
