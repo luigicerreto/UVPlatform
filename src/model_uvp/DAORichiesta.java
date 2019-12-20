2349bb7901d790a607b7c95b3f921b08e8299331
rosar <rosar@172.19.192.243>
2019-12-20 15:18:41 +0100

    Aggiunto metodo in DAORichiesta e aggiunta servlet per rifiutare le richieste.

--- src/model_uvp/DAORichiesta.java
+++ src/model_uvp/DAORichiesta.java
@@ -655,5 +655,35 @@ public class DAORichiesta {
 		}
 		return false;
 	}
+	
+	public boolean rejectByTeach_Company(int idRequest)
+	{
+		final String newState = "Rifiutata e conclusa";
+		Connection con = new DbConnection().getInstance().getConn();
+		PreparedStatement statement = null;
+		ResultSet result;
+		String updateS = "UPDATE request_internship\r\n" + 
+				"SET STATE = ?\r\n" + 
+				"WHERE id_request_i = ?;";
+		try {
+			statement = con.prepareStatement(updateS);
+			statement.setString(1, newState);
+			statement.setInt(2, idRequest);
+			if(statement.executeUpdate()>0)
+			{
+				con.commit();
+				return true;
+			}
+			else
+			{
+				con.rollback();
+				return false;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
 
 }
