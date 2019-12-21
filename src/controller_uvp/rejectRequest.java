def0257a57383efc9a7bb38d9e0816e27ea967ca
Carmine <carmine@192.168.15.105>
2019-12-21 18:14:10 +0100

    Implementate le funzioni "Accetta" e "Rifiuta" in docente

--- src/controller_uvp/rejectRequest.java
+++ src/controller_uvp/rejectRequest.java
@@ -19,35 +19,36 @@ import model_uvp.DAORichiesta;
 @WebServlet("/rejectRequest")
 public class rejectRequest extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public rejectRequest() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public rejectRequest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@SuppressWarnings("unchecked")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		Integer result = 0;
 		String error = "";
 		String content = "";
 		String redirect = "";
-		int id_request = (Integer.parseInt(request.getParameter("choice")));
+
+		int id_request = (Integer.parseInt(request.getParameter("id_request")));
 		DAORichiesta queryobj = new DAORichiesta();
-		
+
 		if(queryobj.rejectByTeach_Company(id_request))
 		{
 			result = 1;
@@ -58,9 +59,10 @@ public class rejectRequest extends HttpServlet {
 			result = 0;
 			error = "Errore nell'elaborazione della richiesta";
 		}
-		
-		
+
+
 		JSONObject res = new JSONObject();
+		
 		res.put("result", result);
 		res.put("error", error);
 		res.put("content", content);
