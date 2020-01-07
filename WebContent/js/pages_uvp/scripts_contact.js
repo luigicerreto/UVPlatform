d3401f8f33b879d0fa509948c309b5641f6adff6
Antonio <Antonio@172.19.201.79>
2020-01-07 11:55:18 +0100

    Correzione redirect della pagina di contatto

--- WebContent/js/pages_uvp/scripts_contact.js
+++ WebContent/js/pages_uvp/scripts_contact.js
@@ -1,30 +1,42 @@
-$(document).ready(function() {
-	$(document).on('submit','#Contact',function() {
-		var object = $("#oggetto").val();
-		var text = $("#textareaContact").val();
-		$.ajax({
-			url : absolutePath + "/contact",
-			type : "POST",
-			dataType : 'JSON',
-			async : false,
-			data : 
+$(document).ready(function() 
+		{
+	$(document).on('submit','#Contact',function() 
 			{
-				"object" : object,
-				"text" : text,
-			},
-			success : function(msg) {
-				if (!msg.result) {
-					showAlert(1,msg.error);
-				} else {
-					showAlert(0,msg.content);
-					setTimeout(function() {
-						window.location.href = msg.redirect;
-					},500);
-				}
-			},
-			error : function(msg) {
-				showAlert(1,"Impossibile inviare l'email.");
-			}
-		});
-	});
-});
+			var object = $("#oggetto").val();
+			var text = $("#textareaContact").val();
+		$.ajax(
+				{
+					url : absolutePath + "/contact",
+					type : "POST",
+					dataType : 'JSON',
+					async : false,
+					data : 
+					{
+					"object" : object,
+					"text" : text,
+					},
+					success : function(msg) 
+					{
+						if (!msg.result) 
+						{
+							showAlert(1,msg.error);
+						} 
+						else 
+						{
+							showAlert(0,msg.content);
+							setTimeout(function() 
+									{
+								window.location.href = msg.redirect;
+									},
+									500);
+						}
+					},
+					error : function(msg) 
+					{
+						showAlert(1,"Impossibile inviare l'email.");
+					}
+				});
+		$(".preloader").hide();
+		return false;
+			});
+		}); 
