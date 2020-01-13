07dd03d9c0b72343734c64ee970d9c436c4b553e
Antonio <a.baldi20@studenti.unisa.it>
2020-01-13 15:05:08 +0100

    Completati test riguardanti il pacchetto Model_uvp

--- src/test/Uvp_DAOUserTest.java
+++ src/test/Uvp_DAOUserTest.java
@@ -4,12 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import controller.DbConnection;
+import controller.Utils;
 import model_uvp.DAOInternship;
 import model_uvp.DAOUser;
 import model_uvp.User;
@@ -21,36 +24,46 @@ class Uvp_DAOUserTest {
 	{
 		app = new DAOUser();
 	}
-	
+
 	@Test
 	void testUpdatePasswordFail() throws SQLException {
 		app.updatePassword("", "testing");
 	}
-	
+
 	@Test
 	void testUpdatePasswordPass() throws SQLException {
 		Connection con = new DbConnection().getInstance().getConn();
 		PreparedStatement statement = null;
+		ResultSet result;
 		String addUser = "INSERT INTO user VALUES ('a.baldi20@studenti.unisa.it', 'Antonio', 'Baldi', 'M', 'password', 0, '0512105521', 'null', 'null' )";
 		statement = con.prepareStatement(addUser);
 		statement.executeUpdate();
 		con.commit();
-		
+
 		app.updatePassword("a.baldi20@studenti.unisa.it", "password123");
-		
+		String selectUser = "SELECT PASSWORD FROM USER WHERE EMAIL='a.baldi20@studenti.unisa.it'";
+		statement = con.prepareStatement(selectUser);
+		result=statement.executeQuery();
+		String psw = new Utils().generatePwd("password123");
+		if(result.next())
+		{
+			assertEquals(psw, result.getString(1));
+		}
+
+
 		String DeleteUser = "DELETE FROM user WHERE email = 'a.baldi20@studenti.unisa.it'";
 		statement = con.prepareStatement(DeleteUser);
 		statement.executeUpdate();
 		con.commit();
 	}
-	
-	
+
+
 
 	@Test
 	void testCheckMailFail() throws SQLException {
 		app.checkMail("");
 	}
-	
+
 	@Test
 	void testCheckMailPass() throws SQLException {
 		Connection con = new DbConnection().getInstance().getConn();
@@ -59,9 +72,9 @@ class Uvp_DAOUserTest {
 		statement = con.prepareStatement(addUser);
 		statement.executeUpdate();
 		con.commit();
-		
-		app.checkMail("a.baldi20@studenti.unisa.it");
-		
+
+		assertEquals(true,app.checkMail("a.baldi20@studenti.unisa.it"));
+
 		String DeleteUser = "DELETE FROM user WHERE email = 'a.baldi20@studenti.unisa.it'";
 		statement = con.prepareStatement(DeleteUser);
 		statement.executeUpdate();
@@ -80,9 +93,20 @@ class Uvp_DAOUserTest {
 		statement = con.prepareStatement(addUser);
 		statement.executeUpdate();
 		con.commit();
-		
-		app.getUser("a.baldi20@studenti.unisa.it");
-		
+		User utente;
+		utente=app.getUser("a.baldi20@studenti.unisa.it");
+
+		assertEquals("a.baldi20@studenti.unisa.it", utente.getEmail());
+		assertEquals("Antonio", utente.getName());
+		assertEquals("Baldi", utente.getSurname());
+		assertEquals('M', utente.getSex());
+		assertEquals("password", utente.getPassword());
+		assertEquals(0, utente.getUserType());
+		assertEquals("0512105521", utente.getSerial());
+
+
+
+
 		String DeleteUser = "DELETE FROM user WHERE email = 'a.baldi20@studenti.unisa.it'";
 		statement = con.prepareStatement(DeleteUser);
 		statement.executeUpdate();
@@ -93,7 +117,7 @@ class Uvp_DAOUserTest {
 	void testGetUserByRequestInternshipFail() {
 		app.getUserByRequestInternship(0);
 	}
-	
+
 	@Test
 	void testGetUserByRequestInternshipPass() throws SQLException {
 		Connection con = new DbConnection().getInstance().getConn();
@@ -112,7 +136,15 @@ class Uvp_DAOUserTest {
 		statement = con.prepareStatement(addRequest);
 		statement.executeUpdate();
 		con.commit();
-		app.getUserByRequestInternship(111);
+		User utente=app.getUserByRequestInternship(111);
+
+		assertEquals("a.baldi20@studenti.unisa.it", utente.getEmail());
+		assertEquals("Antonio", utente.getName());
+		assertEquals("Baldi", utente.getSurname());
+		assertEquals('M', utente.getSex());
+		assertEquals("password", utente.getPassword());
+		assertEquals(0, utente.getUserType());
+		assertEquals("0512105521", utente.getSerial());
 
 
 		String DeleteRequest = "DELETE FROM request_internship WHERE ID_REQUEST_I = 111";
@@ -145,9 +177,17 @@ class Uvp_DAOUserTest {
 		statement = con.prepareStatement(addRequestEV);
 		statement.executeUpdate();
 		con.commit();
-		
-		app.getUserByRequestEV(111);
-		
+
+		User utente=app.getUserByRequestEV(111);
+
+		assertEquals("a.baldi20@studenti.unisa.it", utente.getEmail());
+		assertEquals("Antonio", utente.getName());
+		assertEquals("Baldi", utente.getSurname());
+		assertEquals('M', utente.getSex());
+		assertEquals("password", utente.getPassword());
+		assertEquals(0, utente.getUserType());
+		assertEquals("0512105521", utente.getSerial());
+
 		String DeleteUser = "DELETE FROM user WHERE email = 'a.baldi20@studenti.unisa.it'";
 		String DeleteRequest = "DELETE FROM request WHERE ID_REQUEST = 111";
 		statement = con.prepareStatement(DeleteRequest);
@@ -169,21 +209,30 @@ class Uvp_DAOUserTest {
 		statement = con.prepareStatement(addUser);
 		statement.executeUpdate();
 		con.commit();
+
+		ArrayList<User> utenti=app.viewUsers();
+		User utente = utenti.get(0);
+		assertEquals("a.baldi20@studenti.unisa.it", utente.getEmail());
+		assertEquals("Antonio", utente.getName());
+		assertEquals("Baldi", utente.getSurname());
+		assertEquals('M', utente.getSex());
+		assertEquals("password", utente.getPassword());
+		assertEquals(0, utente.getUserType());
+		assertEquals("0512105521", utente.getSerial());
 		
-		app.viewUsers();
-		
+
 		String DeleteUser = "DELETE FROM user WHERE email = 'a.baldi20@studenti.unisa.it'";
 		statement = con.prepareStatement(DeleteUser);
 		statement.executeUpdate();
 		con.commit();
-		
+
 	}
 
 	@Test
 	void testViewTeachersFail() {
 		app.viewTeachers();
 	}
-	
+
 	@Test
 	void testViewTeachersPass() throws SQLException {
 		Connection con = new DbConnection().getInstance().getConn();
@@ -192,9 +241,21 @@ class Uvp_DAOUserTest {
 		statement = con.prepareStatement(addUser);
 		statement.executeUpdate();
 		con.commit();
-		
-		app.viewTeachers();
-		
+
+		ArrayList<User> utenti=app.viewTeachers();
+		for(User utente : utenti)
+		{
+		if(utente.getEmail().equals("a.baldi20@studenti.unisa.it"))
+		{
+		assertEquals("a.baldi20@studenti.unisa.it", utente.getEmail());
+		assertEquals("Antonio", utente.getName());
+		assertEquals("Baldi", utente.getSurname());
+		assertEquals('M', utente.getSex());
+		assertEquals("password", utente.getPassword());
+		assertEquals(3, utente.getUserType());
+		}
+		}
+
 		String DeleteUser = "DELETE FROM user WHERE email = 'a.baldi20@studenti.unisa.it'";
 		statement = con.prepareStatement(DeleteUser);
 		statement.executeUpdate();
@@ -205,7 +266,7 @@ class Uvp_DAOUserTest {
 	void testViewCompaniesFail() {
 		app.viewCompanies();
 	}
-	
+
 	@Test
 	void testViewCompaniesPass() throws SQLException {
 		Connection con = new DbConnection().getInstance().getConn();
@@ -214,9 +275,19 @@ class Uvp_DAOUserTest {
 		statement = con.prepareStatement(addUser);
 		statement.executeUpdate();
 		con.commit();
-		
-		app.viewCompanies();
-		
+
+		ArrayList<User> utenti=app.viewCompanies();
+		for(User utente : utenti)
+		{
+		if(utente.getEmail().equals("a.baldi20@studenti.unisa.it"))
+		{
+		assertEquals("a.baldi20@studenti.unisa.it", utente.getEmail());
+		assertEquals("Antonio", utente.getName());
+		assertEquals("password", utente.getPassword());
+		assertEquals(4, utente.getUserType());
+		}
+		}
+
 		String DeleteUser = "DELETE FROM user WHERE email = 'a.baldi20@studenti.unisa.it'";
 		statement = con.prepareStatement(DeleteUser);
 		statement.executeUpdate();
@@ -231,13 +302,22 @@ class Uvp_DAOUserTest {
 	void testEditUserPass() throws SQLException {
 		Connection con = new DbConnection().getInstance().getConn();
 		PreparedStatement statement;
+		ResultSet result;
 		String addUser = "INSERT INTO user VALUES ('a.baldi20@studenti.unisa.it', 'Antonio', 'Baldi', 'M', 'password', 0, '0512105521', 'null', 'null' )";
 		statement = con.prepareStatement(addUser);
 		statement.executeUpdate();
 		con.commit();
-		
+
 		app.editUser("a.baldi20@studenti.unisa.it", "PHONE", "3922412988");
 		
+		String selectPhone = "SELECT PHONE FROM USER WHERE EMAIL='a.baldi20@studenti.unisa.it'";
+		statement = con.prepareStatement(selectPhone);
+		result=statement.executeQuery();
+		if(result.next())
+		{
+		assertEquals("3922412988", result.getString(1));
+		}
+
 		String DeleteUser = "DELETE FROM user WHERE email = 'a.baldi20@studenti.unisa.it'";
 		statement = con.prepareStatement(DeleteUser);
 		statement.executeUpdate();
@@ -254,6 +334,7 @@ class Uvp_DAOUserTest {
 	void testAddTeacherPass() throws SQLException {
 		Connection con = new DbConnection().getInstance().getConn();
 		PreparedStatement statement;
+		ResultSet result;
 		User prova = new User();
 		prova.setEmail("cgravino@unisa.it");
 		prova.setName("Carmine");
@@ -263,11 +344,25 @@ class Uvp_DAOUserTest {
 		prova.setOffice("f2");
 		app.addTeacher(prova);
 		
+		String selectUser = "SELECT * FROM USER WHERE EMAIL='cgravino@unisa.it'";
+		statement = con.prepareStatement(selectUser);
+		result=statement.executeQuery();
+		if(result.next())
+		{
+			assertEquals("cgravino@unisa.it", result.getString(1));
+			assertEquals("Carmine", result.getString(2));
+			assertEquals("Gravino", result.getString(3));
+			assertEquals("M", result.getString(4));
+			assertEquals(new Utils().generatePwd("testing123"), result.getString(5));
+			assertEquals(3, result.getInt(6));
+		}
+		
+
 		String DeleteUser = "DELETE FROM user WHERE email = 'cgravino@unisa.it'";
 		statement = con.prepareStatement(DeleteUser);
 		statement.executeUpdate();
 		con.commit();
-		
+
 	}
 
 	@Test
@@ -276,11 +371,12 @@ class Uvp_DAOUserTest {
 		prova.setPassword("testing");
 		app.addCompany(prova);
 	}
-	
+
 	@Test
 	void testAddCompanyPass() throws SQLException {
 		Connection con = new DbConnection().getInstance().getConn();
 		PreparedStatement statement;
+		ResultSet result;
 		User prova = new User();
 		prova.setEmail("info@david.it");
 		prova.setName("david");
@@ -288,6 +384,17 @@ class Uvp_DAOUserTest {
 		prova.setOffice("napoli");
 		app.addCompany(prova);
 		
+		String selectUser = "SELECT * FROM USER WHERE EMAIL='info@david.it'";
+		statement = con.prepareStatement(selectUser);
+		result=statement.executeQuery();
+		if(result.next())
+		{
+			assertEquals("info@david.it", result.getString(1));
+			assertEquals("david", result.getString(2));
+			assertEquals(null, result.getString(4));
+			assertEquals(new Utils().generatePwd("testing123"), result.getString(5));
+			assertEquals(4, result.getInt(6));
+		}
 		String DeleteUser = "DELETE FROM user WHERE email = 'info@david.it'";
 		statement = con.prepareStatement(DeleteUser);
 		statement.executeUpdate();
@@ -298,19 +405,25 @@ class Uvp_DAOUserTest {
 	void testRemoveUserFail() {
 		app.removeUser("");
 	}
-	
+
 	@Test
 	void testRemoveUserPass() throws SQLException {
 		Connection con = new DbConnection().getInstance().getConn();
 		PreparedStatement statement;
+		ResultSet result;
 		String addUser = "INSERT INTO user VALUES ('a.baldi20@studenti.unisa.it', 'Antonio', 'Baldi', 'M', 'password', 0, '0512105521', 'null', 'null' )";
 		statement = con.prepareStatement(addUser);
 		statement.executeUpdate();
 		con.commit();
-		
+
 		app.removeUser("a.baldi20@studenti.unisa.it");
 		
-		
+		String selectUser = "SELECT * FROM USER WHERE EMAIL='a.baldi20@studenti.unisa.it'";
+		statement = con.prepareStatement(selectUser);
+		result=statement.executeQuery();
+			assertTrue(!result.first());
+
+
 	}
 
 }
